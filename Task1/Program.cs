2f6eb6f986f7f1f1e299a23ace8a919de7f8f4cb
Anton2725 <anton545@mail.ru>
2021-10-21 21:06:59 +0500

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static string path = "h:\\projects\\CDEV-9\\temp\\Text1.txt";
+        static void Main(string[] args)
+        {
+            // Задание 1
+            // Наша задача — сравнить производительность вставки в List<T> и LinkedList<T>.Для этого используйте уже знакомый вам StopWatch.
+            // На примере этого текста, выясните, какие будут различия между этими коллекциями.
+
+
+            string text = File.ReadAllText(path);
+            string noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            char[] separators = { ' ', '\n' };
+            string[] words = noPunctuationText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            var listWords = new List<string>();
+            var linkedListWords = new LinkedList<string>();
+
+            var stopWatch = Stopwatch.StartNew();
+            foreach (var word in words)
+            {
+                //Console.Write($"{word} || ");
+                listWords.Add(word);
+            }
+            // смотрим, сколько операция заняла, в миллисекундах
+            Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+            // Примерно 2 мс.
+
+            stopWatch = Stopwatch.StartNew();
+            foreach (var word in words)
+            {
+                //linkedListWords.AddLast(word);
+                linkedListWords.AddFirst(word);
+            }
+            // смотрим, сколько операция заняла, в миллисекундах
+            Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+            // Примерно 14 мс.
+
+            // Вывод - вставка в List быстрее чем в LinkedList.
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Task2
+{
+    class Program
+    {
+        static string path = "h:\\projects\\CDEV-9\\temp\\Text1.txt";
+        static void Main(string[] args)
+        {
+            // Задание 2
+            // Ваша задача — написать программу, которая позволит понять, какие 10 слов чаще всего встречаются в тексте.
+
+
+            string text = File.ReadAllText(path);
+            string noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            char[] separators = { ' ', '\n' };
+            string[] words = noPunctuationText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            var counter = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (counter.ContainsKey(word))
+                {
+                    counter[word]++;
+                }
+                else
+                {
+                    counter.Add(word, 1);
+                }
+            }
+
+            var listTop = counter.Values.ToList<int>();
+            listTop.Sort();
+            listTop.Reverse();
+            var hsTop = listTop.ToHashSet();
+
+            int countTop = 1;
+            foreach (var el in hsTop)
+            {
+                Console.WriteLine($"Место-{countTop} - употребляется в тексте {el} раз:");
+
+                foreach (var word in counter)
+                {
+                    if (word.Value == el)
+                    {
+                        Console.WriteLine($"    {word.Key}");
+                    }
+                }
+
+                countTop++;
+
+                if (countTop > 10) break;
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
